c5d12263559357c74370ed4ecbb6514487343e15
Julian Curalli <julian.curalli27@gmail.com>
2025-06-08 16:31:38 -0400

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL("https://aufrb49ors.ufs.sh/f/**")],
+    remotePatterns: [
+      new URL("https://aufrb49ors.ufs.sh/f/**"),
+      new URL("https://kdib81rtmm.ufs.sh/f/**"),
+    ],
   },
 };
 
