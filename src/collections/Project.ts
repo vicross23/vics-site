69d5949819d9ef3c7bf4e690a1321aa24923e14d
Julian Curalli <julian.curalli27@gmail.com>
2026-04-26 15:26:36 -0400

    added access to projects collcetion

--- src/collections/Project.ts
+++ src/collections/Project.ts
@@ -8,6 +8,12 @@ import {
 export const Project: CollectionConfig = {
   slug: "projects",
   admin: { useAsTitle: "name" },
+  access: {
+    read: () => true,
+    create: () => true,
+    update: () => true,
+    delete: () => true,
+  },
   hooks: {
     afterChange: [syncProjectImagesToMedia],
     afterDelete: [removeProjectFromMedia],
